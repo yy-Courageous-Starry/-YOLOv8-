994bccd181ad06bbeb12be25c86719dba60df07b
UltralyticsAssistant <web@ultralytics.com>
2026-09-04 02:09:12 +0000

    Auto-format by https://ultralytics.com/actions

--- 02预训练模型推理摄像头.py
+++ 02预训练模型推理摄像头.py
@@ -1,13 +1,13 @@
-#1.导入YOLO类
+# 1.导入YOLO类
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-#2.加载模块
+if __name__ == "__main__":
+    # 2.加载模块
     model = YOLO("yolo26n.pt")
 
-#3.设置检测的资源
+# 3.设置检测的资源
 source = 0
-#4.模型推理 --- predict方法
+# 4.模型推理 --- predict方法
 """
     predict方法的参数，可以在官网手册中找到，重要的如下：
     1.source：检测的资源
@@ -16,9 +16,4 @@ source = 0
     4.device：推理的设备，GPU写0
 返回值只有一个，是列表
 """
-results = model.predict(
-    source=source,
-    save= True,
-    device = 0,
-    show = True
-)
+results = model.predict(source=source, save=True, device=0, show=True)

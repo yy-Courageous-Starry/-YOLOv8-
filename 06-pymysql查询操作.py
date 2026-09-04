4094da1980c3d76192051d4215c970968022e318
UltralyticsAssistant <web@ultralytics.com>
2026-09-04 01:56:42 +0000

    Auto-format by https://ultralytics.com/actions

--- 06-pymysql查询操作.py
+++ 06-pymysql查询操作.py
@@ -1,28 +1,23 @@
 import pymysql
 
 conn = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="Yuan13888057275",
-    database="yolo26",
-    charset="utf8"
+    host="localhost", port=3306, user="root", password="Yuan13888057275", database="yolo26", charset="utf8"
 )
 
 cur = conn.cursor()
 
-#查询条件sql语法：select * from 表名 where 列名=值
-#%s是占位符
+# 查询条件sql语法：select * from 表名 where 列名=值
+# %s是占位符
 sql = "select * from user where username=%s"
 
-cur.execute(sql,args=["cc"])
+cur.execute(sql, args=["cc"])
 
 result = cur.fetchall()
 
 cur.close()
 conn.close()
 
-if len(result)>0:
+if len(result) > 0:
     print("账号已存在")
 else:
     print("账号不存在")
